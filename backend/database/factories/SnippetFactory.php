ae51d55dc0eace644003c9b7a5ef550c0d70efb8
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-03-23 22:51:06 +0200

    Added factories and seeds

---
+++ backend/database/factories/SnippetFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Snippet>
+ */
+class SnippetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'content' => fake()->text(10),
+            'language' => fake()->text(10),
+            'keywords' => fake()->text(10)
+        ];
+    }
+}

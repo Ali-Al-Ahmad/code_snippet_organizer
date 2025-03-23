1edb0d09d18b43ad9ee0f775d32316a3456d2b16
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-03-23 23:15:08 +0200

    Added routes and protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Signup from './pages/Signup/Signup'
+import Login from './pages/Login/Login'
+import Home from './pages/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+import Upload from './pages/Upload/Upload'
+import ProtectedRoutes from './utils/ProtectedRoutes'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState('')
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar
+        search={search}
+        setSearch={setSearch}
+      />
+
+      <Routes>
+        <Route
+          path='/'
+          element={<Login />}
+        ></Route>
+        <Route
+          path='/signup'
+          element={<Signup />}
+        ></Route>
+        <Route element={<ProtectedRoutes />}>
+          <Route
+            path='/home'
+            element={<Home search={search} />}
+          />
+
+          <Route
+            path='/upload'
+            element={<Upload />}
+          />
+        </Route>
+      </Routes>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
+import { Fragment } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Fragment>
+    <ToastContainer
+      autoClose={2000}
+      position='top-center'
+    />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Fragment>
 )
